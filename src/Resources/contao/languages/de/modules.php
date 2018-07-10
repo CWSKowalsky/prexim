9ebed49ba98e1d000aedec4a8042820e6b20f963
SKowalsky <skowalsky37@gmail.com>
2018-07-10 17:14:46 +0200

    updated language

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['TL_LANG']['MOD']['prex'][0] = 'Produkte Export';
+$GLOBALS['TL_LANG']['MOD']['prex'][0] = 'Produkt-Export';
 $GLOBALS['TL_LANG']['MOD']['prex'][1] = 'Export-Vorlagen verwalten';
 $GLOBALS['TL_LANG']['FMD']['prex_list'][0] = 'Export-Vorlagen';
 $GLOBALS['TL_LANG']['FMD']['prex_list'][1] = 'Auflistung der Export-Vorlagen';
