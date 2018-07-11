02d704b6469ead138b088d45ab2feb3303478cd5
SKowalsky <skowalsky37@gmail.com>
2018-07-11 10:03:24 +0200

    updated template & upload messages

--- src/Resources/contao/modules/prim.php
+++ src/Resources/contao/modules/prim.php
@@ -6,13 +6,12 @@
 		
 		protected function compile() 
 		{
+			$this->Template->sts = 'Status: Warten auf Upload';
 			$this->checkUpload();
-			$this->Template->sts = 'Status: Import ausstehend';
-			$this->Template->sts = 'Status: Import abgeschlossen';
 		}
 
 		public function checkUpload() {
-			$target_dir = $_SERVER['DOCUMENT_ROOT']."uploads/";
+			$target_dir = "webuploads/";
 			if (!file_exists($target_dir)) { mkdir($target_dir, 0777, true); }
 			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 			$uploadOk = 0;
@@ -22,12 +21,21 @@
 					$uploadOk = 1;
 				}
 				if($uploadOk == 1 && move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-					$this->Template->scc = "Die Datei wurde erfolgreich hochgeladen!";
+					$import = file_get_contents($target_file);
+					$importarr = unserialize($import);
+					$pram = sizeof($importarr['products']);
+					$vaam = sizeof($importarr['variants']);
+					if($pram > 0 || $vaam > 0) {
+						$this->Template->sts = 'Status: Datei hochgeladen';
+						$this->Template->scc = "Die Datei wurde erfolgreich hochgeladen (Produkte: $pram, Varianten: $vaam)";
+						$this->Temyplate->shimpbtn = true;
+					} else {
+						$this->Template->sts = 'Status: Datei hochgeladen (fehlerhaft)';
+						$this->Template->err = "Es wurden keine Produkte/Varianten in der Datei gefunden.";
+					}
 				} else {
 					$this->Template->err = "Die Datei konnte nicht hochgeladen werden.";
 				}
-				$import = file_get_contents($target_file);
-				$this->import($import);
 			}
 		}
 
