fed707bcbdac02a919f958411e422eedce4e666b
SKowalsky <skowalsky37@gmail.com>
2018-07-11 10:20:58 +0200

    added check import

--- src/Resources/contao/modules/prim.php
+++ src/Resources/contao/modules/prim.php
@@ -8,15 +8,16 @@
 		{
 			$this->Template->sts = 'Status: Warten auf Upload';
 			$this->checkUpload();
+			$this->checkImport();
 		}
 
 		public function checkUpload() {
-			$target_dir = "webuploads/";
-			if (!file_exists($target_dir)) { mkdir($target_dir, 0777, true); }
-			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-			$uploadOk = 0;
-			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 			if(isset($_POST["submit"])) {
+				$target_dir = "webuploads/";
+				if (!file_exists($target_dir)) { mkdir($target_dir, 0777, true); }
+				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+				$uploadOk = 0;
+				$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 				if($imageFileType == "prex") {
 					$uploadOk = 1;
 				}
@@ -29,6 +30,7 @@
 						$this->Template->sts = 'Status: Datei hochgeladen';
 						$this->Template->scc = "Die Datei wurde erfolgreich hochgeladen (Produkte: $pram, Varianten: $vaam)";
 						$this->Temyplate->shimpbtn = true;
+						$this->Temyplate->ifile = $target_file;
 					} else {
 						$this->Template->sts = 'Status: Datei hochgeladen (fehlerhaft)';
 						$this->Template->err = "Es wurden keine Produkte/Varianten in der Datei gefunden.";
@@ -39,8 +41,30 @@
 			}
 		}
 
-		public function import($import) {
-			$this->Template->sts = 'Status: Import ausstehend: '.strlen($import);
+		public function checkImport() {
+			if(isset($_POST['isubmit'])) {
+				if(isset($_POST['ifile']) && file_exists($_POST['ifile'])) {
+					$import = file_get_contents($target_file);
+					if(isset($import) && $import != '') {
+						$importarr = unserialize($import);
+						if(sizeof($importarr) > 0) {
+							if(isset($importarr['products']) && isset($importarr['variants'])) {
+								$pram = sizeof($importarr['products']);
+								$vaam = sizeof($importarr['variants']);
+								$this->import($importarr);
+								$this->Template->sts = 'Status: Import abgeschlossen';
+								$this->Template->scc = "Import erfolgreich (Produkte: $pram, Varianten: $vaam)";
+							}
+						}
+					}
+				}
+			} else {
+				$this->Template->err = "Fehler beim importieren.";
+			}
+		}
+
+		public function import($importarr) {
+			//TODO
 		}
 
 	}
